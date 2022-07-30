4fd01a0a5be0e4101f29cc9f7975eb351f5c0ce5
cyan92128505 <cyan92128505@gmail.com>
2022-07-30 21:23:56 +0800

    Project initial

---
+++ lib/components/contact_detail.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
+
+class ContactDetail extends StatelessWidget {
+  final Contact contact;
+  const ContactDetail({
+    Key? key,
+    required this.contact,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(contact.displayName)),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('First name: ${contact.name.first}'),
+          Text('Last name: ${contact.name.last}'),
+          Text(
+              'Phone number: ${contact.phones.isNotEmpty ? contact.phones.first.number : '(none)'}'),
+          Text(
+              'Email address: ${contact.emails.isNotEmpty ? contact.emails.first.address : '(none)'}'),
+        ],
+      ),
+    );
+  }
+}
